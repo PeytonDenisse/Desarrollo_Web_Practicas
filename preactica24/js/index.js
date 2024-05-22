8404c9e4273bba1768c09118225177c70ef62c9a
PeytonDenisse <dcv78658@lasalle.edu.mx>
2024-05-22 07:37:17 -0600

    mejoras

--- preactica24/js/index.js
+++ preactica24/js/index.js
@@ -11,7 +11,7 @@ let tareas = [
         fecha: "2021-09-01",
         descripcion: "Es un mapache",
         imagen: "",
-        video: "https://www.w3schools.com/html/mov_bbb.mp4",
+        video: "https://youtu.be/OvhVLFQmgOY",
         audio: "",
         completada: false
     },
@@ -20,7 +20,7 @@ let tareas = [
         fecha: "2021-09-02",
         descripcion: "Chihuahua",
         imagen: "",
-        video: "https://www.w3schools.com/html/mov_bbb.mp4",
+        video: "https://youtu.be/OvhVLFQmgOY",
         audio: "",
         completada: false
     },
@@ -29,13 +29,13 @@ let tareas = [
         fecha: "2021-09-03",
         descripcion: "Shiba Inu",
         imagen: "",
-        video: "https://www.w3schools.com/html/mov_bbb.mp4",
+        video: "https://youtu.be/OvhVLFQmgOY",
         audio: "",
         completada: false
     }
 ];
 
-// mostrar las tareas
+// Mostrar las tareas
 function mostrarTareas() {
     listaTareas.innerHTML = "";
     tareas.forEach((tarea, indice) => {
@@ -51,7 +51,7 @@ function mostrarTareas() {
                     ${tarea.imagen ? `<img src="${tarea.imagen}" class="card-img mb-2" alt="Imagen">` : ""}
                 </div>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">${tarea.video ? `<video controls autoplay muted src="${tarea.video}" class="card-video mb-2" style="width: 100%; height: auto;"></video>` : ""}</li>
+                    <li class="list-group-item">${tarea.video ? `<iframe class="card-video mb-2" width="100%" height="200" src="${convertirUrlYouTube(tarea.video)}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>` : ""}</li>
                     <li class="list-group-item">${tarea.audio ? `<audio controls src="${tarea.audio}" class="card-audio w-100 mb-2"></audio>` : ""}</li>
                     <li class="list-group-item">
                         <div class="form-check">
@@ -75,11 +75,17 @@ function mostrarTareas() {
     });
 }
 
+function convertirUrlYouTube(url) {
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    const match = url.match(regExp);
+    return (match && match[2].length == 11) ? `https://www.youtube.com/embed/${match[2]}` : null;
+}
+
 function marcarCompletada(indice, checkbox) {
     tareas[indice].completada = checkbox.checked;
 }
 
-//agregar una tarea
+// Agregar una tarea
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
     agregarDatos();
@@ -88,7 +94,7 @@ formulario.addEventListener("submit", (e) => {
     formulario.reset();
 });
 
-//llenar el formulario de edición
+// Llenar el formulario de edición
 let llenarFormularioEditar = (indice) => {
     document.getElementById("indiceEditar").value = indice;
     document.getElementById("nombreEditar").value = tareas[indice].nombre;
@@ -99,7 +105,7 @@ let llenarFormularioEditar = (indice) => {
     document.getElementById("audioUrlEditar").value = tareas[indice].audio;
 };
 
-// guardar los cambios de la tarea editada
+// Guardar los cambios de la tarea editada
 formularioEditar.addEventListener("submit", (e) => {
     e.preventDefault();
     editarTarea();
@@ -107,7 +113,7 @@ formularioEditar.addEventListener("submit", (e) => {
     mostrarTareas();
 });
 
-// agregar datos
+// Agregar datos
 let agregarDatos = () => {
     const imagenUrl = document.getElementById("imagenUrl").value;
     const imagenArchivo = document.getElementById("imagenArchivo").files[0];
@@ -126,7 +132,7 @@ let agregarDatos = () => {
     });
 };
 
-// editar una tarea
+// Editar una tarea
 let editarTarea = () => {
     let indice = document.getElementById("indiceEditar").value;
     const imagenUrl = document.getElementById("imagenUrlEditar").value;
@@ -144,7 +150,7 @@ let editarTarea = () => {
     tareas[indice].audio = document.getElementById("audioUrlEditar").value;
 };
 
-//borrar una tarea
+// Borrar una tarea
 let borrarTarea = (indice) => {
     if (confirm("¿Estás seguro de eliminar esta tarea?")) {
         tareas.splice(indice, 1);
@@ -152,7 +158,7 @@ let borrarTarea = (indice) => {
     }
 };
 
-// cerrar el modal
+// Cerrar el modal
 let cerrarModal = (idModal) => {
     let modal = document.getElementById(idModal);
     let bsModal = bootstrap.Modal.getInstance(modal);
